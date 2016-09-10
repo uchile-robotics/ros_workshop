69942101aad2a102c55e2f42c0316d9fc24f332a
Rodrigo Muñoz <rorro.mr@gmail.com>
2016-09-10 00:57:38 -0300

    Example follow file

--- ros_workshop/src/ros_workshop/follow.py
+++ ros_workshop/src/ros_workshop/follow.py
@@ -14,36 +14,8 @@ class Follow(object):
         self.command = Twist()
         
     def process_laser(self, msg):
-        # Filter
-        ranges = []
-        angles = []
         for i, distance in enumerate(msg.ranges):
-            if distance < msg.range_max*0.9:
-                ranges.append(distance)
-                angles.append(msg.angle_min+msg.angle_increment*i)
-        if not ranges or not angles:
-            return # Avoid empty data
-        # Process
-        est_distance = np.mean(ranges)
-        est_angle = np.mean(angles)
-        # Calc command
-        error_distance = est_distance - 0.5
-        error_angle = est_angle - 0.0
-        # Calc command using saturation limits
-        linear_cmd = Follow.saturation(0.2*error_distance, -0.2, 0.2)
-        angular_cmd = Follow.saturation(0.4*error_angle, -0.5, 0.5)
-        # Set command
-        self.command.linear.x = linear_cmd
-        self.command.angular.z = angular_cmd
-        self.base_pub.publish(self.command)
-    
-    @staticmethod
-    def saturation(value, min_value, max_value):
-        if value > max_value:
-            return max_value
-        if value < min_value:
-            return min_value
-        return value
+            print distance
 
 if __name__ == '__main__':
     rospy.init_node('p2')
